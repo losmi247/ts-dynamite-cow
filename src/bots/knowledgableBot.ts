274c7fb04ece27a16deae11a678a52949b50abf4
Materol <gaarapavel@gmail.com>
2024-07-08 15:16:38 +0100

    Finished bot to do proportional randomness based on other player's moves

--- src/bots/knowledgableBot.ts
+++ src/bots/knowledgableBot.ts
@@ -2,85 +2,54 @@ import { Gamestate, BotSelection } from '../models/gamestate';
 
 class Bot {
     private p2Statistics= new opponentStatistics();
+    private dynamiteCount = 0;
     makeMove(gamestate: Gamestate): BotSelection {
         this.p2Statistics.updateStatistics(gamestate);
-        if(this.p2Statistics.totalRockMovesProportion > 0.5) return 'P';
-        else if(this.p2Statistics.totalPaperMovesProportion > 0.5) return 'S';
-        else if(this.p2Statistics.totalScissorsMovesProportion > 0.5) return 'R';
-        else if(this.p2Statistics.totalDynamiteMovesProportion > 0.5) return 'W';
-        else if(this.p2Statistics.totalWaterBalloonMovesProportion > 0.5) return 'P';
-        else return 'S';
+        const randomRoll = Math.random() + 0.1;
+        if(randomRoll < this.p2Statistics.getProportionalTotalMoves('R')){
+            return 'P';
+        }
+        else if(randomRoll < this.p2Statistics.getProportionalTotalMoves('R') + this.p2Statistics.getProportionalTotalMoves('P')){
+            return 'S';
+        }
+        else if(randomRoll < this.p2Statistics.getProportionalTotalMoves('R') + this.p2Statistics.getProportionalTotalMoves('P') + this.p2Statistics.getProportionalTotalMoves('S')){
+            return 'R';
+        }
+        else{
+            if(this.dynamiteCount<100){
+                this.dynamiteCount++;
+                return 'D';
+            }
+            return 'P';
+        }
+
     }
 }
 
 export = new Bot();
 
 class opponentStatistics {
-    public totalMoves: number;
+    public totalMoves: number = 0;
 
-    public totalRockMoves: number;
-    public totalPaperMoves: number;
-    public totalScissorsMoves: number;
-    public totalWaterBalloonMoves: number;
-    public totalDynamiteMoves: number;
-
-    public totalRockMovesProportion: number;
-    public totalPaperMovesProportion: number;
-    public totalScissorsMovesProportion: number;
-    public totalWaterBalloonMovesProportion: number;
-    public totalDynamiteMovesProportion: number;
-
+    public totalMovesDictionary:{[K in BotSelection]:number} = {
+            'R': 0,
+            'P': 0,
+            'S': 0,
+            'D': 0,
+            'W': 0
+    };
 
     public constructor() {
-        this.totalMoves = 0;
-        this.totalRockMoves = 0;
-        this.totalPaperMoves = 0;
-        this.totalScissorsMoves = 0;
-        this.totalWaterBalloonMoves = 0;
-        this.totalDynamiteMoves = 0;
     }
 
     public updateStatistics(gamestate : Gamestate){
         if(gamestate.rounds.length === 0) return;
-        const opponentPreviousMove = gamestate.rounds[gamestate.rounds.length - 1].p2;
-        this.updateTotalMoves(opponentPreviousMove);
-        this.updateMovesPercentage();
-    }
-
-    private updateTotalMoves(move: BotSelection){
+        const opponentPreviousMoves: BotSelection = gamestate.rounds[gamestate.rounds.length - 1].p2;
+        this.totalMovesDictionary[opponentPreviousMoves]++;
         this.totalMoves++;
-        switch(move){
-            case 'R': {
-                this.totalRockMoves++;
-                break;
-            }
-            case 'P': {
-                this.totalPaperMoves++;
-                break;
-            }
-            case 'S': {
-                this.totalScissorsMoves++;
-                break;
-            }
-            case 'D': {
-                this.totalScissorsMoves++;
-                break;
-            }
-            case 'W': {
-                this.totalWaterBalloonMoves++;
-                break;
-            }
-            default: {
-                break;
-            }
-        }
     }
 
-    private updateMovesPercentage(){
-        this.totalRockMovesProportion = this.totalRockMoves / this.totalMoves;
-        this.totalPaperMovesProportion = this.totalPaperMoves / this.totalMoves;
-        this.totalScissorsMovesProportion = this.totalScissorsMoves / this.totalMoves;
-        this.totalDynamiteMovesProportion = this.totalDynamiteMoves / this.totalMoves;
-        this.totalWaterBalloonMovesProportion = this.totalWaterBalloonMoves / this.totalMoves;
+    public getProportionalTotalMoves(move : BotSelection){
+        return this.totalMovesDictionary[move] / this.totalMoves;
     }
 }
