8e7dbf1f91f7a4c0eca2fb87fce9085e21e458c5
Amy Siripoonya <amy.siripoonya@softwire.com>
2024-07-08 14:23:25 +0100

    rebased onto milos, updated mysteryBot to have dynamite threshold

--- src/bots/mysteryBotMilos.ts
+++ src/bots/mysteryBotMilos.ts
@@ -1,13 +1,12 @@
 import {BotSelection, Gamestate} from '../models/gamestate';
 
 const moves: BotSelection[] = ['R', 'P', 'S', 'D', 'W'];
+const drawThreshold = 4;
 
 class Bot {
-    dynamitesUsed: number;
+    dynamitesUsed: number = 0;
+    consecutiveDraws: number = 0;
 
-    constructor() {
-        this.dynamitesUsed = 0;
-    }
 
     getRandomIntInRange(low: number, high: number) {
         const lowCeiled = Math.ceil(low);
@@ -36,7 +35,21 @@ class Bot {
             return this.getRandomMove();
         }
 
+        if (this.consecutiveDraws == drawThreshold) {
+            if (this.dynamitesUsed < 100) {
+                this.dynamitesUsed++;
+                this.consecutiveDraws = 0;
+                return "D"
+            }
+            this.consecutiveDraws = 0;
+        }
+
         let previousOpponentMoves = gameState.rounds.map((r) => r.p2);
+        let previousPlayerMoves =  gameState.rounds.map((r) => r.p1);
+
+        if (previousPlayerMoves[previousPlayerMoves.length-1] == previousOpponentMoves[previousOpponentMoves.length-1] ) {
+            this.consecutiveDraws++;
+        }
 
         let moveCounts = {'R': 1, 'P': 1, 'S': 1, 'D': 1, 'W': 1};
         previousOpponentMoves.forEach((move) => moveCounts[move]++);
